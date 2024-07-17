1061d1629e1772e596f361a6cfaf6530fd7f4001
Shikder-Tarek-15 <shikdertarek2019@gmail.com>
2024-07-17 21:43:03 +0600

    Add cash in functionality

--- src/Dashboard/UserDashboard/CashIn.jsx
+++ src/Dashboard/UserDashboard/CashIn.jsx
@@ -2,8 +2,7 @@ import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import useAuth from "../../Hooks/useAuth";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-
-const SendMoney = () => {
+const CashIn = () => {
   const { user } = useAuth();
   const axiosPublic = useAxiosPublic();
   const {
@@ -11,32 +10,34 @@ const SendMoney = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = async (data) => {
-    try {
-      const sendData = { ...data, id: user._id };
-      const response = await axiosPublic.post("/send", sendData);
-      console.log(response);
-      // TODO: check inserted id ache nki cashout ew
-      toast.success("Money sent successfully");
-    } catch (err) {
-      console.log(err);
-      const errorMessage = err.response?.data?.error || "An error occurred";
-      toast.error(errorMessage);
-    }
+  const onSubmit = (data) => {
+    const cashOutData = { ...data, id: user._id };
+    axiosPublic
+      .post("/cashin", cashOutData)
+      .then((res) => {
+        console.log(res);
+        toast.success("Cash in request successful");
+      })
+      .catch((err) => {
+        console.log(err);
+        // Extracting the error message
+        const errorMessage = err.response?.data?.error || "An error occurred";
+        toast.error(errorMessage);
+      });
   };
-  console.log(user);
+
   return (
     <div className="w-full max-w-md p-8 space-y-3 rounded-xl dark:bg-gray-50 dark:text-gray-800 mx-auto mt-12">
-      <h1 className="text-2xl font-bold text-center mb-6">Send Money</h1>
+      <h1 className="text-2xl font-bold text-center mb-6">Cashout</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         <div className="space-y-1 text-sm">
           <label htmlFor="username" className="block dark:text-gray-600">
-            Enter Receiver Number
+            Enter Agent Number
           </label>
           <input
             type="text"
             name="number"
-            placeholder="Receiver Number"
+            placeholder="Agent Number"
             {...register("number", { required: true })}
             className="w-full px-4 py-3 rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600 border"
           />
@@ -46,7 +47,7 @@ const SendMoney = () => {
         </div>
         <div className="space-y-1 text-sm">
           <label htmlFor="username" className="block dark:text-gray-600">
-            Enter Sending amount
+            Enter Casn Out amount
           </label>
           <input
             type="number"
@@ -78,7 +79,7 @@ const SendMoney = () => {
           type="submit"
           className="block w-full p-3 text-center rounded-sm dark:text-gray-50 dark:bg-violet-600"
         >
-          Send Money
+          Cashout
         </button>
       </form>
       <Toaster />
@@ -86,4 +87,4 @@ const SendMoney = () => {
   );
 };
 
-export default SendMoney;
+export default CashIn;
